c77c01046e25f63d70bf30d6e6b1240c0556a443
lautarolopez01 <lauchalopez4@gmail.com>
2023-07-20 18:41:25 -0300

    TokenScanner funcionando

--- spellchecker/src/main/java/edu/isistan/spellchecker/tokenizer/TokenScanner.java
+++ spellchecker/src/main/java/edu/isistan/spellchecker/tokenizer/TokenScanner.java
@@ -1,69 +1,110 @@
 package edu.isistan.spellchecker.tokenizer;
 
-import java.util.Iterator;
 import java.io.IOException;
+import java.io.Reader;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
- * Dado un archivo provee un mÈtodo para recorrerlo.
+ * Dado un archivo provee un m√©todo para recorrerlo.
  */
 public class TokenScanner implements Iterator<String> {
+    private Reader reader;
+    private int currentChar;
 
-  /**
-   * Crea un TokenScanner.
-   * <p>
-   * Como un iterador, el TokenScanner solo debe leer lo justo y
-   * necesario para implementar los mÈtodos next() y hasNext(). 
-   * No se debe leer toda la entrada de una.
-   * <p>
-   *
-   * @param in fuente de entrada
-   * @throws IOException si hay alg˙n error leyendo.
-   * @throws IllegalArgumentException si el Reader provisto es null
-   */
-  public TokenScanner(java.io.Reader in) throws IOException {
-  
-  }
+    /**
+     * Crea un TokenScanner.
+     * <p>
+     * Como un iterador, el TokenScanner solo debe leer lo justo y
+     * necesario para implementar los m√©todos next() y hasNext().
+     * No se debe leer toda la entrada de una.
+     * <p>
+     *
+     * @param in fuente de entrada
+     * @throws IOException              si hay alg√∫n error leyendo.
+     * @throws IllegalArgumentException si el Reader provisto es null
+     */
+    public TokenScanner(Reader in) throws IOException {
+        if (in == null) {
+            throw new IllegalArgumentException("El Reader provisto es null.");
+        }
+        reader = in;
+        currentChar = reader.read();
+    }
 
-  /**
-   * Determina si un car·cer es una caracter v·lido para una palabra.
-   * <p>
-   * Un caracter v·lido es una letra (
-   * Character.isLetter) o una apostrofe '\''.
-   *
-   * @param c 
-   * @return true si es un caracter
-   */
-  public static boolean isWordCharacter(int c) {
-    return false;
-  }
+    /**
+     * Determina si un car√°cer es una caracter v√°lido para una palabra.
+     * <p>
+     * Un caracter v√°lido es una letra (
+     * Character.isLetter) o una apostrofe '\''.
+     *
+     * @param c
+     * @return true si es un caracter
+     */
+    public static boolean isWordCharacter(int c) {
+        return Character.isLetter(c) || c == '\'';
+    }
 
+    /**
+     * Determina si un string es una palabra v√°lida.
+     * Null no es una palabra v√°lida.
+     * Un string que todos sus caracteres son v√°lidos es una
+     * palabra. Por lo tanto, el string vac√≠o es una palabra v√°lida.
+     *
+     * @param s
+     * @return true si el string es una palabra.
+     */
+    public static boolean isWord(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+        for (char c : s.toCharArray()) {
+            if (!isWordCharacter(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-   /**
-   * Determina si un string es una palabra v·lida.
-   * Null no es una palabra v·lida.
-   * Un string que todos sus caracteres son v·lidos es una 
-   * palabra. Por lo tanto, el string vacÌo es una palabra v·lida.
-   * @param s 
-   * @return true si el string es una palabra.
-   */
-  public static boolean isWord(String s) {
-		return false;
-  }
+    /**
+     * Determina si hay otro token en el reader.
+     */
+    public boolean hasNext() {
+        return currentChar != -1;
+    }
 
-  /**
-   * Determina si hay otro token en el reader.
-   */
-  public boolean hasNext() {
-    return false;
-  }
+    /**
+     * Retorna el siguiente token.
+     *
+     * @throws NoSuchElementException cuando se alcanz√≥ el final de stream
+     */
+    public String next() {
+        StringBuilder tokenBuilder = new StringBuilder();
+        try {
+            while (currentChar != -1 && isWordCharacter(currentChar)) {
+                tokenBuilder.append((char) currentChar);
+                currentChar = reader.read();
+            }
 
-  /**
-   * Retorna el siguiente token.
-   *
-   * @throws NoSuchElementException cuando se alcanzÛ el final de stream
-   */
-  public String next() {
-    return null;
-  }
+            if (tokenBuilder.length() == 0) {
+                while (currentChar != -1 && !isWordCharacter(currentChar)) {
+                    tokenBuilder.append((char) currentChar);
+                    currentChar = reader.read();
+                }
 
+                if (tokenBuilder.length() == 0) {
+                    tokenBuilder.append((char) currentChar);
+                    currentChar = reader.read();
+                }
+            }
+
+            if (tokenBuilder.length() == 0) {
+                throw new NoSuchElementException("No hay m√°s tokens disponibles.");
+            }
+
+            return tokenBuilder.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Error al leer el siguiente token.", e);
+        }
+    }
 }
