32df13bd14a4e3279f6f775821aec072128fd474
lautarolopez01 <lauchalopez4@gmail.com>
2023-07-25 18:41:50 -0300

    File corrector listo

--- spellchecker/src/main/java/edu/isistan/spellchecker/corrector/impl/FileCorrector.java
+++ spellchecker/src/main/java/edu/isistan/spellchecker/corrector/impl/FileCorrector.java
@@ -1,115 +1,54 @@
 package edu.isistan.spellchecker.corrector.impl;
 
-import java.util.Set;
-
 import edu.isistan.spellchecker.corrector.Corrector;
 
 import java.io.*;
+import java.util.*;
 
-/**
- * Corrector basado en un archivo.
- * 
- */
 public class FileCorrector extends Corrector {
 
-	/** Clase especial que se utiliza al tener 
-	 * algún error de formato en el archivo de entrada.
-	 */
+	private Map<String, Set<String>> correctionsMap;
+
 	public static class FormatException extends Exception {
 		public FormatException(String msg) {
 			super(msg);
 		}
 	}
 
-
-	/**
-	 * Constructor del FileReader
-	 *
-	 * Utilice un BufferedReader para leer el archivo de definición
-	 *
-	 * <p> 
-	 * Cada línea del archivo del diccionario tiene el siguiente formato: 
-	 * misspelled_word,corrected_version
-	 *
-	 * <p>
-	 *Ejemplo:<br>
-	 * <pre>
-	 * aligatur,alligator<br>
-	 * baloon,balloon<br>
-	 * inspite,in spite<br>
-	 * who'ev,who've<br>
-	 * ther,their<br>
-	 * ther,there<br>
-	 * </pre>
-	 * <p>
-	 * Estas líneas no son case-insensitive, por lo que todas deberían generar el mismo efecto:<br>
-	 * <pre>
-	 * baloon,balloon<br>
-	 * Baloon,balloon<br>
-	 * Baloon,Balloon<br>
-	 * BALOON,balloon<br>
-	 * bAlOon,BALLOON<br>
-	 * </pre>
-	 * <p>
-	 * Debe ignorar todos los espacios vacios alrededor de las palabras, por lo
-	 * que estas entradas son todas equivalentes:<br>
-	 * <pre>
-	 * inspite,in spite<br>
-	 *    inspite,in spite<br>
-	 * inspite   ,in spite<br>
-	 *  inspite ,   in spite  <br>
-	 * </pre>
-	 * Los espacios son permitidos dentro de las sugerencias. 
-	 *
-	 * <p>
-	 * Debería arrojar <code>FileCorrector.FormatException</code> si se encuentra algún
-	 * error de formato:<br>
-	 * <pre>
-	 * ,correct<br>
-	 * wrong,<br>
-	 * wrong correct<br>
-	 * wrong,correct,<br>
-	 * </pre>
-	 * <p>
-	 *
-	 * @param r Secuencia de caracteres 
-	 * @throws IOException error leyendo el archivo
-	 * @throws FileCorrector.FormatException error de formato
-	 * @throws IllegalArgumentException reader es null
-	 */
 	public FileCorrector(Reader r) throws IOException, FormatException {
-
-	}
-
-	/** Construye el Filereader.
-	 *
-	 * @param filename 
-	 * @throws IOException 
-	 * @throws FileCorrector.FormatException 
-	 * @throws FileNotFoundException 
-	 */
-	public static FileCorrector make(String filename) throws IOException, FormatException {
-		Reader r = new FileReader(filename);
-		FileCorrector fc;
-		try {
-			fc = new FileCorrector(r);
-		} finally {
-			if (r != null) { r.close(); }
+		if (r == null) {
+			throw new IllegalArgumentException("El Reader no puede ser null.");
 		}
-		return fc;
+		correctionsMap = new TreeMap<>();
+
+		try (BufferedReader br = new BufferedReader(r)) {
+			String line;
+			int lineNumber = 1;
+			while ((line = br.readLine()) != null) {
+				line = line.trim();
+				if (!line.isEmpty()) {
+					String[] parts = line.split(",", 2);
+					if (parts.length != 2 || parts[0].trim().isEmpty() || parts[1].trim().isEmpty()) {
+						throw new FormatException("Error de formato en lÃ­nea " + lineNumber + ": " + line);
+					}
+					String wrong = parts[0].trim().toLowerCase();
+					String correct = parts[1].trim();
+					correctionsMap.computeIfAbsent(wrong, k -> new TreeSet<>()).add(correct);
+				}
+				lineNumber++;
+			}
+		}
 	}
 
-	/**
-	 * Retorna una lista de correcciones para una palabra dada.
-	 * Si la palabra mal escrita no está en el diccionario el set es vacio.
-	 * <p>
-	 * Ver superclase.
-	 *
-	 * @param wrong 
-	 * @return retorna un conjunto (potencialmente vacío) de sugerencias.
-	 * @throws IllegalArgumentException si la entrada no es una palabra válida 
-	 */
+	public static FileCorrector make(String filename) throws IOException, FormatException {
+		try (Reader r = new FileReader(filename)) {
+			return new FileCorrector(r);
+		}
+	}
+
 	public Set<String> getCorrections(String wrong) {
-		return null;
+		String wrongLowerCase = wrong.trim().toLowerCase();
+		Set<String> corrections = correctionsMap.getOrDefault(wrongLowerCase, Collections.emptySet());
+		return matchCase(wrong, corrections);
 	}
 }
