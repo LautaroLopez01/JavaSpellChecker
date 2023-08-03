1f63aeb6edab9f4b9522530a52bcb62dbe153e67
lautarolopez01 <lauchalopez4@gmail.com>
2023-08-03 18:52:19 -0300

    MyTests

--- spellchecker/src/test/java/edu/isistan/spellchecker/MyTests.java
+++ spellchecker/src/test/java/edu/isistan/spellchecker/MyTests.java
@@ -1,5 +1,7 @@
 package edu.isistan.spellchecker;
+import edu.isistan.spellchecker.corrector.Corrector;
 import edu.isistan.spellchecker.corrector.Dictionary;
+import edu.isistan.spellchecker.corrector.impl.FileCorrector;
 import edu.isistan.spellchecker.corrector.impl.SwapCorrector;
 import edu.isistan.spellchecker.tokenizer.TokenScanner;
 import org.junit.*;
@@ -7,6 +9,8 @@ import org.junit.*;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.StringReader;
+import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -104,5 +108,136 @@ public class MyTests {
         } finally {
             reader.close();
         }
+    }
+
+    // Token Scanner tests
+    @Test
+    public void testEmptyInput() throws IOException {
+        String input = "";
+        TokenScanner scanner = new TokenScanner(new StringReader(input));
+        assertFalse(scanner.hasNext());
+    }
+
+    @Test
+    public void testSingleWordToken() throws IOException {
+        String input = "hello";
+        TokenScanner scanner = new TokenScanner(new StringReader(input));
+        assertTrue(scanner.hasNext());
+        assertEquals("hello", scanner.next());
+        assertFalse(scanner.hasNext());
+    }
+
+    @Test
+    public void testSingleNonWordToken() throws IOException {
+        String input = "123";
+        TokenScanner scanner = new TokenScanner(new StringReader(input));
+        assertTrue(scanner.hasNext());
+        assertEquals("123", scanner.next());
+        assertFalse(scanner.hasNext());
+    }
+
+    @Test
+    public void testMixedTokensEndingWithWord() throws IOException {
+        String input = "Hello, world!";
+        TokenScanner scanner = new TokenScanner(new StringReader(input));
+        assertTrue(scanner.hasNext());
+        assertEquals("Hello", scanner.next());
+        assertTrue(scanner.hasNext());
+        assertEquals(",", scanner.next());
+        assertTrue(scanner.hasNext());
+        assertEquals("world", scanner.next());
+        assertTrue(scanner.hasNext());
+        assertEquals("!", scanner.next());
+        assertFalse(scanner.hasNext());
+    }
+
+    @Test
+    public void testMixedTokensEndingWithNonWord() throws IOException {
+        String input = "Hello, world!!!";
+        TokenScanner scanner = new TokenScanner(new StringReader(input));
+        assertTrue(scanner.hasNext());
+        assertEquals("Hello", scanner.next());
+        assertTrue(scanner.hasNext());
+        assertEquals(",", scanner.next());
+        assertTrue(scanner.hasNext());
+        assertEquals("world", scanner.next());
+        assertTrue(scanner.hasNext());
+        assertEquals("!!!", scanner.next());
+        assertFalse(scanner.hasNext());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testNoSuchElementException() throws IOException {
+        String input = "Hello,";
+        TokenScanner scanner = new TokenScanner(new StringReader(input));
+        while (scanner.hasNext()) {
+            scanner.next();
+        }
+        // The next call to scanner.next() should throw NoSuchElementException
+        scanner.next();
+    }
+
+    // Tests File Corrector
+
+    @Test
+    public void testFileWithExtraSpaces() throws IOException, FileCorrector.FormatException {
+        String inputFileContent = "  palabra1   ,   correccion1  \n" +
+                "palabra2,correccion2  \n" +
+                "  palabra3  ,   correccion3   ";
+        FileCorrector corrector = new FileCorrector(new StringReader(inputFileContent));
+
+        Set<String> corrections1 = corrector.getCorrections("palabra1");
+        assertEquals(1, corrections1.size());
+        assertTrue(corrections1.contains("correccion1"));
+
+        Set<String> corrections2 = corrector.getCorrections("palabra2");
+        assertEquals(1, corrections2.size());
+        assertTrue(corrections2.contains("correccion2"));
+
+        Set<String> corrections3 = corrector.getCorrections("palabra3");
+        assertEquals(1, corrections3.size());
+        assertTrue(corrections3.contains("correccion3"));
+    }
+
+    @Test
+    public void testWordWithoutCorrections() throws IOException, FileCorrector.FormatException {
+        String inputFileContent = "palabra,correccion";
+        FileCorrector corrector = new FileCorrector(new StringReader(inputFileContent));
+
+        Set<String> corrections = corrector.getCorrections("inexistente");
+        assertTrue(corrections.isEmpty());
+    }
+
+    @Test
+    public void testWordWithMultipleCorrections() throws IOException, FileCorrector.FormatException {
+        String inputFileContent =
+                "palabra,correccion1\n" +
+                        "palabra,correccion2\n" +
+                        "palabra,correccion3";
+        FileCorrector corrector = new FileCorrector(new StringReader(inputFileContent));
+
+        Set<String> corrections = corrector.getCorrections("palabra");
+
+        assertEquals(3, corrections.size());
+        assertTrue(corrections.contains("correccion1"));
+        assertTrue(corrections.contains("correccion2"));
+        assertTrue(corrections.contains("correccion3"));
+    }
+
+    @Test
+    public void testDistintasCapitalizacionesFileCorrector() throws IOException,FileCorrector.FormatException {
+        Corrector c = FileCorrector.make("misspellings.txt");
+        assertEquals("surPRiZe -> {surprise}", makeSet(new String[]{"surprise"}), c.getCorrections("surPRiZe"));
+        assertEquals("SUrPRiZe -> {Surprise}", makeSet(new String[]{"Surprise"}), c.getCorrections("SUrPRiZe"));
+        assertEquals("surPRiZe -> {surprise}", makeSet(new String[]{"surprise"}), c.getCorrections("surPRiZe"));
+    }
+
+    // Metodo auxiliar para comparaciones
+    private Set<String> makeSet(String[] strings) {
+        Set<String> mySet = new TreeSet<String>();
+        for (String s : strings) {
+            mySet.add(s);
+        }
+        return mySet;
     }
 }
